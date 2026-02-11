5207bef7f87c48ca25ae479cbfcbd54bf8db1b77
Nilesh Vyas <nileshhvyas@gmail.com>
2026-02-11 18:34:19 +0530

    tailwind css added

--- FULLSTACK DEVELOPMENT/REACT/01_Start/src/App.jsx
+++ FULLSTACK DEVELOPMENT/REACT/01_Start/src/App.jsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <>
-      <h1>Welcome to React</h1>
+      <h1 class='text-3xl font-bold underline'>Welcome to React</h1>
       <p>I am learning react because I want to buy new car in 2026.</p>
     </>
   )
