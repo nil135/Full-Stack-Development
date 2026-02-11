6f01e2a9a2f5811d99d110b3a9ad4be7d4ce4abb
Nilesh Vyas <nileshhvyas@gmail.com>
2026-02-11 17:57:49 +0530

    Changes updated

---
+++ REACT/01_Start/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Welcome to React</h1>
+      <p>I am learning react because I want to buy new car in 2026.</p>
+    </>
+  )
+}
+
+export default App
